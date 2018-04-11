386a49b959010a29a2f1cf27b14f374776aca555
Odixi <vz4ev4r@gmail.com>
2018-04-11 15:59:25 +0300

    Made a quick test

---
+++ Assets/Scripts/BallMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMover : MonoBehaviour {
+
+	public Vector3 startPosition;
+	public Vector2 velocity;
+	public float g = 9.81f;
+	public bool simulate = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void ThrowBall(float vel, float ang){
+		velocity = Vector2.right * vel;
+		velocity = Quaternion.Euler (0, 0, ang) * velocity;
+		simulate = true;
+	}
+
+	void FixedUpdate () {
+		if (simulate) {
+			//gravity
+			velocity.y = velocity.y - g * Time.fixedDeltaTime; 
+
+			// moving of the ball
+			float x = transform.position.x + velocity.x * Time.fixedDeltaTime;
+			float y = transform.position.y + velocity.y * Time.fixedDeltaTime;
+			transform.position = new Vector3 (x, y, 0);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		ResetPosition ();
+		simulate = false;
+	}
+
+	public void ResetPosition(){
+		transform.position = startPosition;
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text velocityField;
+	public Text angleField;
+	public GameObject ball;
+	private BallMover ballMover;
+	public Button throwBtn;
+
+
+	// Use this for initialization
+	void Start () {
+		throwBtn.onClick.AddListener (onThrowClicked);
+		ballMover = ball.GetComponent<BallMover> ();
+	}
+		
+	void onThrowClicked(){
+		ballMover.ResetPosition ();
+		ballMover.ThrowBall (float.Parse (velocityField.text), float.Parse (angleField.text));
+		print ("throw");
+	}
+
+}
